b1ed9834e67a82931f868481d16dde95a9f13be8
lty <1838623164@qq.com>
2020-03-17 22:20:55 +0800

    1

---
+++ src/Getallclass.java
@@ -0,0 +1,25 @@
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Getallclass {
+	InQueue inQueue = new InQueue();
+	public static void GotoOne(String gitPath ,String commitID) {
+	}
+
+	public static void shasha(String gitPath) {	
+	}
+	public static void main(String[] args) {
+		Getallclass a = new Getallclass(); 
+		a.inQueue.put();
+	}
+
+}
+class InQueue{
+	public void put() {
+		System.out.println("lty");
+	}
+}
+
+
